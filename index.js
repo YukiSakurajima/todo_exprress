6cf5cce61d1b9d381076ec23ab17c8d4a60810d6
aaron <aaron.malton@voco.ee>
2024-10-19 20:07:18 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+     // get data from file
+     fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        // tasks list data from file
+        const tasks = data.split("\n") 
+        res.render('index', {tasks: tasks})})
+})
 
 app.listen(3001, () => {
     console.log('Server started at localhost:3001');
